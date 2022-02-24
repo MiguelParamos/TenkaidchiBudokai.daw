e30e4b6aa9b32fc4a810d3715f2da9f20c4d81b7
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2022-02-24 17:08:42 +0100

    Todo preparado para luchar una ronda
    Ya tenemos armas y combatientes preparados

--- Tenkaidchi.Budokai.DAW/src/main/java/budokai/Tenkaidchi.java
+++ Tenkaidchi.Budokai.DAW/src/main/java/budokai/Tenkaidchi.java
@@ -1,6 +1,7 @@
 package budokai;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import clases.Arma;
 import clases.Combatiente;
@@ -10,17 +11,76 @@ import clases.Pelea;
 public class Tenkaidchi {
 
 	public static void main(String[] args) {
+		//Declaro todas las armas que necesito para empezar el torneo
+		ArrayList<Arma> armasDisponibles=new ArrayList<Arma>();
+		armasDisponibles.add(new Arma("Espada",(byte)16));
+		armasDisponibles.add(new Arma("Ak-47",(byte)20));
+		armasDisponibles.add(new Arma("Chancla de la madre",(byte)12));
+		armasDisponibles.add(new Arma("Daga",(byte)13));
+		armasDisponibles.add(new Arma("Bolso de ladrillos",(byte)16));
+		armasDisponibles.add(new Arma("Guantelete de Thanos",(byte)10));
+		armasDisponibles.add(new Arma("Dedo amputado de ET",(byte)5));
+		armasDisponibles.add(new Arma("Botijo nuclear",(byte)8));
 		
-		Arma espada=new Arma("Espada",(byte)16);
-		ObjetoDefensivo escudo=new ObjetoDefensivo("Escudo",(byte)12);
+		//Declaro todos los objetos defensivos que necesito para empezar el torneo
+		ArrayList<ObjetoDefensivo> objetosDefensivosDisponibles=
+				new ArrayList<ObjetoDefensivo>();
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("escudo",(byte)15));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("capa de caperucita",(byte)6));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("Bolso de un gitano",(byte)5));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("Caparazón de tortuga",(byte)12));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("Paraguas de Mary Poppins",(byte)10));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("sábana",(byte)5));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("antena de 5g",(byte)18));
+		objetosDefensivosDisponibles.add(
+				new ObjetoDefensivo("mascarilla ffp2",(byte)18));
 		
-		Combatiente frodo=
-		new Combatiente("Frodo",espada,escudo);
-		Combatiente wally= 
-		new Combatiente("Wally");
+		//Declaro los nombres de los combatientes
+		String[] nombresDeCombatientes= 
+			{"Rafa","Jose Oña","Jose Coca",
+					"Julia","Berta","Juan Castilla","Emilio","Raul"};
 		
-		Pelea pelea=new Pelea(frodo,wally);
-		pelea.pelear();
+		Random r=new Random();
+		ArrayList<Combatiente> combatientesIniciales=
+				new ArrayList<Combatiente>();
+		
+		for(byte i=0;i<nombresDeCombatientes.length;i++) {
+			//Cojo un arma y un objeto defensivo aleatoriamente de los arrays
+			Arma armaEscogida=armasDisponibles.get(
+					r.nextInt(armasDisponibles.size()));
+			ObjetoDefensivo odEscogido=objetosDefensivosDisponibles.get(
+					r.nextInt(objetosDefensivosDisponibles.size())
+				);
+			
+			//Saco de las disponibles el arma y objeto defensivo escogidos
+			//para que no se repitan
+			armasDisponibles.remove(armaEscogida);
+			objetosDefensivosDisponibles.remove(odEscogido);
+			
+			combatientesIniciales.add(
+					new Combatiente(nombresDeCombatientes[i],
+							armaEscogida,odEscogido)
+			);
+			
+			/*System.out.println("Entra al torneo "+nombresDeCombatientes[i]+
+					"\n\tEscoge : "+armaEscogida+ "\n\ty "+odEscogido);*/
+			System.out.println("Entra al torneo "+
+			combatientesIniciales.get(i)+"\n");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
